6accdfb8c0742a4bf3c8e411db6b3eda472816e5
SANARP98 <33574218+SANARP98@users.noreply.github.com>
2022-10-13 18:39:32 +0530

    Update sarif.go

--- v2/pkg/reporting/exporters/sarif/sarif.go
+++ v2/pkg/reporting/exporters/sarif/sarif.go
@@ -44,7 +44,7 @@ func New(options *Options) (*Exporter, error) {
 		return nil, errors.Wrap(err, "could not template path")
 	}
 
-	run := sarif.NewRunWithInformationURI("nuclei", "https://github.com/projectdiscovery/nuclei")
+	run := sarif.NewRunWithInformationURI("nuclei", "https://github.com/SANARP98/nucleitemplates")
 	return &Exporter{options: options, home: templatePath, sarif: report, run: run, mutex: &sync.Mutex{}}, nil
 }
 
@@ -63,9 +63,9 @@ func (exporter *Exporter) Export(event *output.ResultEvent) error {
 
 	var templateURL string
 	if strings.HasPrefix(event.TemplatePath, exporter.home) {
-		templateURL = "https://github.com/projectdiscovery/nuclei-templates/blob/master" + templatePath
+		templateURL = "https://github.com/SANARP98/nucleitemplates/blob/master" + templatePath
 	} else {
-		templateURL = "https://github.com/projectdiscovery/nuclei-templates"
+		templateURL = "https://github.com//SANARP98/nucleitemplates"
 	}
 
 	var ruleDescription string
