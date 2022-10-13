ad5db88c38a06c5e64557aa75ad1a3f7b95d0eb2
SANARP98 <33574218+SANARP98@users.noreply.github.com>
2022-10-13 19:00:33 +0530

    Update template_path.go

--- v2/pkg/utils/template_path.go
+++ v2/pkg/utils/template_path.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 
 	"github.com/mitchellh/go-homedir"
-	"github.com/projectdiscovery/nuclei-templates/v2/pkg/catalog/config"
+	"github.com/projectdiscovery/nuclei/v2/pkg/catalog/config"
 )
 
 const (
